ae28092d7b22e0cf5448c73112b04144ebc984c7
Dídac García <didaquis@gmail.com>
2019-11-17 18:40:28 +0100

    Fix auth validation

--- src/gql/auth/context.js
+++ src/gql/auth/context.js
@@ -3,9 +3,13 @@
 const { validateAuthToken } = require('./jwt');
 
 const setContext = async ({ req }) => {
-	const token = req.headers['authorization'];
-	if (token) {
+	let token = req.headers['authorization'];
+	if (token && typeof token === 'string') {
 		try {
+			const authenticationScheme = 'Bearer ';
+			if (token.startsWith(authenticationScheme)) {
+				token = token.slice(authenticationScheme.length, token.length);
+			}
 			const user = await validateAuthToken(token);
 			return { user }; // Add to Apollo Server context the user who is doing the request if auth token is provided and it's a valid token
 		} catch (error) {
