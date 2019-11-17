dff0c995ec4f08ec3f54dedc502c6b2ed2b07139
Dídac García <didaquis@gmail.com>
2019-11-17 23:33:20 +0100

    Improved errors on resolvers

--- src/gql/resolvers/auth.js
+++ src/gql/resolvers/auth.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require('apollo-server-express');
+
 const { Users } = require('../../data/models/index');
 const { createAuthToken } = require('../auth/jwt');
 const { authValidations } = require('../auth/validations');
@@ -13,17 +15,20 @@ module.exports = {
 	 */
 	Mutation: {
 		registerUser: async (root, { email, password }) => {
+			if (!email || !password) {
+				throw new AuthenticationError('Data provided is not valid');
+			}
 
 			const numberOfCurrentlyUsersRegistered = await Users.find().estimatedDocumentCount();
 
 			if (authValidations.isLimitOfUsersReached(numberOfCurrentlyUsersRegistered, globalVariablesConfig.limitOfUsersRegistered)) {
-				throw new Error('The maximum number of users allowed has been reached. You must contact the administrator of the service in order to register');
+				throw new AuthenticationError('The maximum number of users allowed has been reached. You must contact the administrator of the service in order to register');
 			}
 
 			const isAnEmailAlreadyRegistered = await Users.findOne({email});
 
 			if (isAnEmailAlreadyRegistered) {
-				throw new Error('Data provided is not valid');
+				throw new AuthenticationError('Data provided is not valid');
 			}
 
 			await new Users({email, password}).save();
@@ -38,16 +43,20 @@ module.exports = {
 		 * It allows users to authenticate. Users with property isActive with value false are not allowed to authenticate. When a user authenticates the value of lastLogin will be updated
 		 */
 		authUser: async (root, { email, password }) => {
+			if (!email || !password) {
+				throw new AuthenticationError('Invalid credentials');
+			}
+
 			const user = await Users.findOne({email, isActive: true});
 
 			if (!user) {
-				throw new Error('User not found or login not allowed');
+				throw new AuthenticationError('User not found or login not allowed');
 			}
 
 			const isCorrectPassword = await bcrypt.compare(password, user.password);
 
 			if (!isCorrectPassword) {
-				throw new Error('Invalid credentials');
+				throw new AuthenticationError('Invalid credentials');
 			}
 
 			await Users.findOneAndUpdate({email}, { lastLogin: new Date().toISOString() }, { new: true });
--- src/gql/resolvers/users.js
+++ src/gql/resolvers/users.js
@@ -1,3 +1,5 @@
+const { ForbiddenError } = require('apollo-server-express');
+
 const { Users } = require('../../data/models/index');
 const { authValidations } = require('../auth/validations');
 
@@ -8,11 +10,11 @@ module.exports = {
 		 */
 		listAllUsers:  async (root, args, context) => {
 			if (!authValidations.isLogged(context)) {
-				throw new Error('You must be logged in to perform this action');
+				throw new ForbiddenError('You must be logged in to perform this action');
 			}
 
 			if (!authValidations.isAdmin(context)) {
-				throw new Error('You must be an administrator to perform this action');
+				throw new ForbiddenError('You must be an administrator to perform this action');
 			}
 
 			const users = await Users.find({});
