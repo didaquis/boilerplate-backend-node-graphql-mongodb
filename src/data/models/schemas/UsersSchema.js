6b7ac96ccc24e07899c2d35906b9c29a46b2bd3b
Dídac García <didaquis@gmail.com>
2019-12-24 18:25:19 +0100

    Fix typo

--- src/data/models/schemas/UsersSchema.js
+++ src/data/models/schemas/UsersSchema.js
@@ -7,7 +7,7 @@ const uuidv4 = require('uuid/v4');
 /**
  * Users schema
  * @constructor Users model constructor
- * @classdesc User have interesting properties. Some of them are isAdmin (false by default), isActive (true by default. Useful for removing login permission to the registered users), uuid (random and uniquee token. Created to provided a random identifier token for every user different than _id native MongoDB value)
+ * @classdesc User have interesting properties. Some of them are isAdmin (false by default), isActive (true by default. Useful for removing login permission to the registered users), uuid (random and unique token. Created to provided a random identifier token for every user different than _id native MongoDB value)
  */
 const UsersSchema = new Schema({
 	email: {
