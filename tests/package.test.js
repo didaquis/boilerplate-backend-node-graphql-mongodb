7b39af4e40de1ea1d06eeb2b4a1818912aed953b
Dídac García <didaquis@gmail.com>
2020-07-27 22:36:47 +0200

    Added some test

---
+++ tests/package.test.js
@@ -0,0 +1,48 @@
+const jsonfile = require('jsonfile');
+
+let packageJSONData; 
+
+describe('package.json file', () => {
+
+	beforeAll(() => {
+		const file = './package.json';
+		packageJSONData = jsonfile.readFileSync(file);
+	});
+
+	test('Should have all dependencies with semver version fixed', () => {
+		if (packageJSONData.dependencies) {
+			const validPattern = /^(\d+\.)(\d+\.)(\d+)$/;
+			const regex = RegExp(validPattern);
+	
+			let allDependenciesAreFixed = true;
+			for (let key in packageJSONData.dependencies){
+				if (Object.prototype.hasOwnProperty.call(packageJSONData.dependencies, key)) {
+					if (!regex.test(packageJSONData.dependencies[key])) {
+						allDependenciesAreFixed = false;
+					}
+				}
+			}
+	
+			expect(allDependenciesAreFixed).toBe(true);
+		}
+	});
+
+	test('Should have all devDependencies with semver version fixed', () => {
+		if (packageJSONData.devDependencies) {
+			const validPattern = /^(\d+\.)(\d+\.)(\d+)$/;
+			const regex = RegExp(validPattern);
+
+			let allDevDependenciesAreFixed = true;
+
+			for (let key in packageJSONData.devDependencies){
+				if (Object.prototype.hasOwnProperty.call(packageJSONData.devDependencies, key)) {
+					if (!regex.test(packageJSONData.devDependencies[key])) {
+						allDevDependenciesAreFixed = false;
+					}
+				}
+			}
+
+			expect(allDevDependenciesAreFixed).toBe(true);
+		}
+	});
+});
---
+++ tests/validations.test.js
@@ -0,0 +1,56 @@
+const { authValidations } = require('../src/gql/auth/validations');
+
+describe('validations', () => {
+	test('Should be an object', () => {
+		const whatTypeIs = (element) => {
+			// eslint-disable-next-line no-magic-numbers
+			return Object.prototype.toString.call(element).slice(8, -1);
+		};
+
+		expect(whatTypeIs(authValidations) === 'Object').toBe(true);
+	});
+
+	describe('isValidEmail', () => {
+		test('Should return false if no receive params', () => {
+			expect(authValidations.isValidEmail()).toBe(false);
+		});
+
+		test('Should return false if no receive a valid data', () => {
+			const fakeData = ['foo', '', 'foo@@foo.foo', 'bar@bar..com', 'biz@biz.', '@foo.mail'];
+
+			fakeData.forEach( data => {
+				expect(authValidations.isValidEmail(data)).toBe(false);
+			});
+		});
+
+		test('Should return true if receive a valid data', () => {
+			const fakeData = ['maria@mail.com', 'john.doe@gmail.com', 'santi72@hotmail.es', 'foo_@yourcompany.it'];
+
+			fakeData.forEach( data => {
+				expect(authValidations.isValidEmail(data)).toBe(true);
+			});
+		});
+	});
+
+	describe('isStrongPassword', () => {
+		test('Should return false if no receive params', () => {
+			expect(authValidations.isStrongPassword()).toBe(false);
+		});
+
+		test('Should return false if no receive a valid data', () => {
+			const fakeData = ['foo', '', '123', 'patata', 'PATATA.', '*', 'Pa1*', 'A2*aaaa', 'abcdefghijkl', 'My-Password', '82569285927659'];
+
+			fakeData.forEach( data => {
+				expect(authValidations.isStrongPassword(data)).toBe(false);
+			});
+		});
+
+		test('Should return true if receive a valid data', () => {
+			const fakeData = ['abcABC123', 'Pat*ta_72', 'Lol#u&8$', 'My-P4ssw0rd', 'w!w_393_MiM', '*ZGw2sEse!uE', 'ByRN*e&M4%xD', 'syW_ntrq-^LEW8D7', 'ExNbNJa8^mtp3f#e', 'gsY*AL-J9A7?-h2c', 'y6XQw+3H4Cef#QCtudX8ZPF5', '34mDnZTPecVpnxmvwGMW9mWY5g', 'jw2AFhSB'];
+
+			fakeData.forEach( data => {
+				expect(authValidations.isStrongPassword(data)).toBe(true);
+			});
+		});
+	});
+});
