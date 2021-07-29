270e075d31dd6d1c67fcf289a71afd1cc8334d1c
Dídac García <didaquis@gmail.com>
2021-07-30 00:11:29 +0200

    Code has been refactored

--- src/gql/auth/authValidations.js
+++ src/gql/auth/authValidations.js
@@ -1,31 +1,13 @@
 'use strict';
 
+const { AuthenticationError, ForbiddenError } = require('apollo-server-express');
+const { Users } = require('../../data/models/index');
+
 /**
  * Auth validations repository
  * @type {Object}
  */
 const authValidations = {
-	/**
-	 * Check if in Apollo Server context contain a logged user 
-	 * @param  {Object} context 		- The context object of Apollo Server
-	 * @param  {Object} [context.user]  - The context object data: user data
-	 * @return {Boolean}
-	 */
-	isLogged: (context) => {
-		return (!context.user) ? false : true;
-	},
-
-	/**
-	 * Check if in Apollo Server context contain a logged user 
-	 * @param  {Object} context 				- The context object of Apollo Server
-	 * @param  {Object} [context.user]  		- The context object data: user data
-	 * @param  {Boolean} [context.user.isAdmin] - The context object data: user data role information
-	 * @return {Boolean}
-	 */
-	isAdmin: (context) => {
-		return (!context.user || !context.user.isAdmin) ? false : true;
-	},
-
 	/**
 	 * Check if the maximum limit of users has been reached
 	 * @param  {Integer} numberOfCurrentlyUsersRegistered 	- The number of users currently registered in the service
@@ -38,6 +20,50 @@ const authValidations = {
 		if (numberOfCurrentlyUsersRegistered >= limitOfUsers) return true;
 
 		return false;
+	},
+
+	/**
+	 * Check if in Apollo Server context contains a logged user. If user is not in context, throw an error
+	 * @param {Object} context 			- The context object of Apollo Server
+	 * @param  {Object} [context.user]  - The context object data: user data
+	 */
+	ensureThatUserIsLogged: (context) => {
+		if (!context.user) {
+			throw new AuthenticationError('You must be logged in to perform this action');
+		}
+	},
+
+	/**
+	 * Check if in Apollo Server context contains an user and is an administrator. If user is not in context or user is not an administrator throw an error
+	 * @param {Object} context 					- The context object of Apollo Server
+	 * @param  {Object} [context.user]  		- The context object data: user data
+	 * @param  {Boolean} [context.user.isAdmin] - The context object data: user data role information
+	 */
+	ensureThatUserIsAdministrator: (context) => {
+		if (!context.user || !context.user.isAdmin) {
+			throw new ForbiddenError('You must be an administrator to perform this action');
+		}
+	},
+
+	/**
+	 * Uses the information in the Apollo Server context to retrieve the user's data from the database. If user does not exist, throw an error.
+	 * @async
+	 * @param {Object} context 					- The context object of Apollo Server
+	 * @param  {Object} [context.user]  		- The context object data: user data
+	 * @returns {User}
+	 */
+	getUser: async (context) => {
+		if (!context.user) {
+			return null;
+		}
+	
+		const uuidOfUser = context.user.uuid || null;
+		const user = await Users.findOne({ uuid: uuidOfUser });
+		if (!user) {
+			throw new AuthenticationError('You must be logged in to perform this action');
+		}
+
+		return user;
 	},
 };
 
--- src/gql/resolvers/users.js
+++ src/gql/resolvers/users.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const { AuthenticationError, ForbiddenError } = require('apollo-server-express');
-
 const { Users } = require('../../data/models/index');
 const { authValidations } = require('../auth/authValidations');
 
@@ -15,13 +13,9 @@ module.exports = {
 		 * It allows to administrators users to list all users registered
 		 */
 		listAllUsers:  async (root, args, context) => {
-			if (!authValidations.isLogged(context)) {
-				throw new AuthenticationError('You must be logged in to perform this action');
-			}
+			authValidations.ensureThatUserIsLogged(context);
 
-			if (!authValidations.isAdmin(context)) {
-				throw new ForbiddenError('You must be an administrator to perform this action');
-			}
+			authValidations.ensureThatUserIsAdministrator(context);
 
 			const users = await Users.find({});
 			return users;
