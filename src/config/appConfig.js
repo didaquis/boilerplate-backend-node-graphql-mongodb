4bf16d2196034bc5f2f922a2304551db93f43561
Dídac García <didaquis@gmail.com>
2023-04-29 00:28:32 +0200

    Fix an issue to connect with database

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -36,7 +36,7 @@ const limitOfUsersRegistered = 0; /* Set the value to 0 to not use the limit. Re
 export const environmentVariablesConfig = Object.freeze({
 	formatConnection: process.env.MONGO_FORMAT_CONNECTION || 'standard',
 	mongoDNSseedlist: process.env.MONGO_DNS_SEEDLIST_CONNECTION || '',
-	dbHost: process.env.MONGO_HOST || 'localhost',
+	dbHost: process.env.MONGO_HOST || '127.0.0.1',
 	dbPort: process.env.MONGO_PORT || '27017',
 	database: process.env.MONGO_DB || 'boilerplate_database',
 	mongoUser: process.env.MONGO_USER || '',
